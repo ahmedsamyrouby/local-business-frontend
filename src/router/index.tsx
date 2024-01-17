abb7c68b9281b2d8ef4fbcc80d1a6ce9e08aabf5
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2024-01-17 21:02:22 +0200

    added ForgotPassword to router

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Login from "../pages/Login/Login";
+import { Routes, Route } from "react-router-dom";
+import Login from "../pages/Authentication/Login/Login";
 import Layout from "../layout";
+import ForgotPassword from "../pages/Authentication/ForgotPassword/ForgotPassword";
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route path="/login" element={<Login />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
       </Route>
     </Routes>
   );
