e21e4183d949ed45711511278630ca58f28db780
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2024-01-19 01:34:56 +0200

    Add forgot password functionality and notification [backend-integration]

--- src/pages/Authentication/ForgotPassword/ForgotPassword.tsx
+++ src/pages/Authentication/ForgotPassword/ForgotPassword.tsx
@@ -2,8 +2,17 @@ import { Title, Text, TextInput, Button } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import AuthenticationLayout from "../AuthenticationLayout/AuthenticationLayout";
 import forgotPasswordArt from "../../../assets/images/forgot-password-art.jpg";
+import { BASE_URL } from "../../../constants";
+import axios from "axios";
+import { useState } from "react";
+import { notifications } from "@mantine/notifications";
+import { IconAlertSquare, IconSquareCheck } from "@tabler/icons-react";
+import { useNavigate } from "react-router-dom";
+import { setLocalStorage } from "../../../services/LocalStorageService";
 
 const ForgotPassword = () => {
+  const navigate = useNavigate();
+
   const form = useForm({
     initialValues: { email: "" },
     validate: {
@@ -11,6 +20,43 @@ const ForgotPassword = () => {
         /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(value) ? null : "Invalid email",
     },
   });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  type FormValues = typeof form.values;
+
+  const handleSendOTP = (values: FormValues) => {
+    setIsLoading(true);
+    axios
+      .post(BASE_URL + "/auth/forgotPassword", values)
+      .then((res) => {
+        setLocalStorage("email", values.email);
+        navigate("/otp");
+        setIsLoading(false);
+        notifications.show({
+          message: "OTP sent successfully, please check your email",
+          autoClose: 2000,
+          icon: <IconSquareCheck />,
+          classNames: {
+            icon: "bg-transparent text-green-500",
+          },
+        });
+      })
+      .catch((err) => {
+        notifications.show({
+          message: err.response.data.message || "Something went wrong",
+          autoClose: 2000,
+          icon: <IconAlertSquare />,
+          classNames: {
+            icon: "bg-transparent text-red-500",
+          },
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <AuthenticationLayout img={forgotPasswordArt}>
       <div className="mb-8">
@@ -24,9 +70,10 @@ const ForgotPassword = () => {
       <div className="w-full">
         <form
           className="space-y-8 md:space-y-10 h-full flex flex-col"
-          onSubmit={form.onSubmit((values) => console.log(values))}
+          onSubmit={form.onSubmit((values) => handleSendOTP(values))}
         >
           <TextInput
+            autoComplete="email"
             placeholder="Enter Email"
             className="text-start"
             classNames={{
@@ -34,7 +81,11 @@ const ForgotPassword = () => {
             }}
             {...form.getInputProps("email")}
           />
-          <Button className="bg-primary w-full text-base" type="submit">
+          <Button
+            className="bg-primary w-full text-base"
+            type="submit"
+            loading={isLoading}
+          >
             {"Send OTP".toUpperCase()}
           </Button>
         </form>
