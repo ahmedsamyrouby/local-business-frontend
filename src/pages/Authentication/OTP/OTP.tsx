ac9f32faf7acad7029f092e636c485c8e9622c40
ahmedsamy2003 <70441494+ahmedsamy2003@users.noreply.github.com>
2024-01-19 14:55:28 +0200

    Add otp verification functionality and notifications [backend-integration]

--- src/pages/Authentication/OTP/OTP.tsx
+++ src/pages/Authentication/OTP/OTP.tsx
@@ -2,14 +2,54 @@ import { PinInput, Title, Text, Button } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import AuthenticationLayout from "../AuthenticationLayout/AuthenticationLayout";
 import forgotPasswordArt from "../../../assets/images/forgot-password-art.jpg";
+import axios from "axios";
+import { BASE_URL } from "../../../constants";
+import { useNavigate } from "react-router-dom";
+import { notifications } from "@mantine/notifications";
+import { IconAlertSquare } from "@tabler/icons-react";
+import { useState } from "react";
 
 const OTP = () => {
+  const navigate = useNavigate();
+
   const form = useForm({
     initialValues: { otp: "" },
     validate: {
       otp: (value) => (value.length < 6 ? "Too short" : null),
     },
   });
+
+  type FormValues = typeof form.values;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleVerifyOTP = (values: FormValues) => {
+    setIsLoading(true);
+    axios
+      .post(BASE_URL + "/auth/verifyResetCode", {
+        email: localStorage.getItem("email"),
+        resetCode: values.otp,
+      })
+      .then((res) => {
+        setIsLoading(false);
+        navigate("/reset-password");
+      })
+      .catch((err) => {
+        console.log(err.response.data.message);
+        notifications.show({
+          message: err.response.data.message || "Something went wrong",
+          autoClose: 2000,
+          icon: <IconAlertSquare />,
+          classNames: {
+            icon: "bg-transparent text-red-500",
+          },
+        });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+
   return (
     <AuthenticationLayout img={forgotPasswordArt}>
       <div className="mb-8">
@@ -22,7 +62,7 @@ const OTP = () => {
       <div className="w-full space-y-8">
         <form
           className="space-y-8 md:space-y-10 flex-center flex-col gap-2.5"
-          onSubmit={form.onSubmit((values) => console.log(values))}
+          onSubmit={form.onSubmit((values) => handleVerifyOTP(values))}
         >
           <PinInput
             classNames={{
@@ -38,7 +78,11 @@ const OTP = () => {
             {...form.getInputProps("otp")}
           />
 
-          <Button className="bg-primary w-full text-base" type="submit">
+          <Button
+            className="bg-primary w-full text-base"
+            type="submit"
+            loading={isLoading}
+          >
             {"Verify".toUpperCase()}
           </Button>
         </form>
